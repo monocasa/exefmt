adaa4e421474878bb69491ed3007c7bfeae1381b
monocasa <tristan.hastings.miller@gmail.com>
2015-08-31 00:13:09 -0600

    Return Strings for EI_CLASS and EI_DATA

--- src/elf.rs
+++ src/elf.rs
@@ -216,11 +216,37 @@ impl ElfFile {
 		Ok(elf)
 	}
 
+	pub fn ehdr_class_string(&self) -> String {
+		ehdr_class_string(self.e_ident[EI_CLASS])
+	}
+
+	pub fn ehdr_data_string(&self) -> String {
+		ehdr_data_string(self.e_ident[EI_DATA])
+	}
+
 }
 
 impl Loader for ElfFile {
 	fn entry_point(&self) -> u64 {
 		self.e_entry
+	}
+}
+
+pub fn ehdr_class_string(e_class: u8) -> String {
+	match e_class {
+		ELFCLASS32 => "ELF32".to_string(),
+		ELFCLASS64 => "ELF64".to_string(),
+
+		_ => format!("Unknown ELF Class {:#x}", e_class),
+	}
+}
+
+pub fn ehdr_data_string(e_data: u8) -> String {
+	match e_data {
+		ELFDATA2LSB => "2's complement, little endian".to_string(),
+		ELFDATA2MSB => "2's complement, big endian".to_string(),
+
+		_ => format!("Unknown ELF Data {:#x}", e_data),
 	}
 }
 
