d13935650e4ced88a8fe5ff9983ca425fa600e7f
monocasa <tristan.hastings.miller@gmail.com>
2015-08-30 22:48:47 -0600

    Parse ELF EHdr

--- src/elffile.rs
+++ src/elffile.rs
@@ -1,4 +1,8 @@
-use std::io::Read;
+extern crate byteorder;
+
+use std::io;
+
+use self::byteorder::{BigEndian, LittleEndian, ReadBytesExt};
 
 use loader::Loader;
 
@@ -30,40 +34,88 @@ pub const EV_CURRENT: u8 = 1;
 pub const EI_NIDENT: usize = 16;
 
 #[derive(Debug)]
-pub enum ElfParseErrors {
-	ReadError,
+pub enum ElfParseError {
+	UnexpectedEOF,
 	InvalidIdent,
+	InvalidVersion,
+	Io(io::Error),
 }
+
+impl From<io::Error> for ElfParseError {
+	fn from(err: io::Error) -> ElfParseError{ ElfParseError::Io(err) }
+}
+
+impl From<byteorder::Error> for ElfParseError {
+	fn from(err: byteorder::Error) -> ElfParseError {
+		match err {
+			byteorder::Error::UnexpectedEOF => ElfParseError::UnexpectedEOF,
+			byteorder::Error::Io(ioerr) => ElfParseError::Io(ioerr),
+		}
+	}
+}
+
+//impl From<byteorder::new::Error> for ElfParseError {
+//	fn from(err: byteorder::new::Error) -> ElfParseError {
+//		match err {
+//			byteorder::new::Error::Io(err) => { ElfParseError::Io(err) }
+//			byteorder::new::Error::UnexpectedEOF => { ElfParseError::UnexpectedEOF }
+//		}
+//	}
+//}
 
 #[derive(Default)]
 pub struct ElfFile {
-	ident: [u8; EI_NIDENT],
-	entry: u64,
+	pub e_ident: [u8; EI_NIDENT],
+	pub e_type: u16,
+	pub e_machine: u16,
+	pub e_version: u32,
+	pub e_entry: u64,
+	pub e_phoff: u64,
+	pub e_shoff: u64,
+	pub e_flags: u32,
+	pub e_ehsize: u16,
+	pub e_phentsize: u16,
+	pub e_phnum: u16,
+	pub e_shentsize: u16,
+	pub e_shnum: u16,
+	pub e_shstrndx: u16,
 }
 
 impl ElfFile {
 	fn new() -> ElfFile {
 		ElfFile {
-			ident: [0u8; EI_NIDENT],
-			entry: 0,
+			e_ident: [0u8; EI_NIDENT],
+			e_type:      0,
+			e_machine:   0,
+			e_version:   0,
+			e_entry:     0,
+			e_phoff:     0,
+			e_shoff:     0,
+			e_flags:     0,
+			e_ehsize:    0,
+			e_phentsize: 0,
+			e_phnum:     0,
+			e_shentsize: 0,
+			e_shnum:     0,
+			e_shstrndx:  0,
 		}
 	}
 
 	fn is_magic_valid(&self) -> bool {
-		(self.ident[EI_MAG0] == ELFMAG0) && (self.ident[EI_MAG1] == ELFMAG1) && 
-			(self.ident[EI_MAG2] == ELFMAG2) && (self.ident[EI_MAG3] == ELFMAG3)
+		(self.e_ident[EI_MAG0] == ELFMAG0) && (self.e_ident[EI_MAG1] == ELFMAG1) && 
+			(self.e_ident[EI_MAG2] == ELFMAG2) && (self.e_ident[EI_MAG3] == ELFMAG3)
 	}
 
 	fn is_class_valid(&self) -> bool {
-		(self.ident[EI_CLASS] == ELFCLASS32) || (self.ident[EI_CLASS] == ELFCLASS64)
+		(self.e_ident[EI_CLASS] == ELFCLASS32) || (self.e_ident[EI_CLASS] == ELFCLASS64)
 	}
 
 	fn is_data_valid(&self) -> bool {
-		(self.ident[EI_DATA] == ELFDATA2LSB) || (self.ident[EI_DATA] == ELFDATA2MSB)
+		(self.e_ident[EI_DATA] == ELFDATA2LSB) || (self.e_ident[EI_DATA] == ELFDATA2MSB)
 	}
 
 	fn is_ver_valid(&self) -> bool {
-		self.ident[EI_VERSION] == EV_CURRENT
+		self.e_ident[EI_VERSION] == EV_CURRENT
 	}
 
 	fn is_ident_valid(&self) -> bool {
@@ -71,31 +123,104 @@ impl ElfFile {
 			self.is_data_valid() && self.is_ver_valid()
 	}
 
-	pub fn read(reader: &mut Read) -> Result<ElfFile, ElfParseErrors> {
+	fn read_u16(&self, rdr: &mut io::Read) -> Result<u16, ElfParseError> {
+		Ok(match self.e_ident[EI_DATA] {
+			ELFDATA2LSB => try!(rdr.read_u16::<LittleEndian>()),
+			ELFDATA2MSB => try!(rdr.read_u16::<BigEndian>()),
+			_ => {
+				return Err(ElfParseError::InvalidIdent);
+			},
+		})
+	}
+
+	fn read_u32(&self, rdr: &mut io::Read) -> Result<u32, ElfParseError> {
+		Ok(match self.e_ident[EI_DATA] {
+			ELFDATA2LSB => try!(rdr.read_u32::<LittleEndian>()),
+			ELFDATA2MSB => try!(rdr.read_u32::<BigEndian>()),
+			_ => {
+				return Err(ElfParseError::InvalidIdent);
+			},
+		})
+	}
+
+	fn read_u64(&self, rdr: &mut io::Read) -> Result<u64, ElfParseError> {
+		Ok(match self.e_ident[EI_DATA] {
+			ELFDATA2LSB => try!(rdr.read_u64::<LittleEndian>()),
+			ELFDATA2MSB => try!(rdr.read_u64::<BigEndian>()),
+			_ => {
+				return Err(ElfParseError::InvalidIdent);
+			},
+		})
+	}
+
+	pub fn read(rdr: &mut io::Read) -> Result<ElfFile, ElfParseError> {
 		let mut elf = ElfFile::new();
 
-		match reader.read(&mut elf.ident) {
+		match rdr.read(&mut elf.e_ident) {
 			Ok(bytes_read) => {
 				if bytes_read != EI_NIDENT {
-					return Err(ElfParseErrors::ReadError);
+					return Err(ElfParseError::UnexpectedEOF);
 				}
 			},
-			Err(_) =>{
-				return Err(ElfParseErrors::ReadError);
+			Err(err) => {
+				return Err(ElfParseError::Io(err));
 			},
 		}
 
 		if !elf.is_ident_valid() {
-			return Err(ElfParseErrors::InvalidIdent);
+			return Err(ElfParseError::InvalidIdent);
+		}
+
+		match elf.e_ident[EI_CLASS] {
+			ELFCLASS32 => {
+				elf.e_type      = try!(elf.read_u16(rdr));
+				elf.e_machine   = try!(elf.read_u16(rdr));
+				elf.e_version   = try!(elf.read_u32(rdr));
+				elf.e_entry     = try!(elf.read_u32(rdr)) as u64;
+				elf.e_phoff     = try!(elf.read_u32(rdr)) as u64;
+				elf.e_shoff     = try!(elf.read_u32(rdr)) as u64;
+				elf.e_flags     = try!(elf.read_u32(rdr));
+				elf.e_ehsize    = try!(elf.read_u16(rdr));
+				elf.e_phentsize = try!(elf.read_u16(rdr));
+				elf.e_phnum     = try!(elf.read_u16(rdr));
+				elf.e_shentsize = try!(elf.read_u16(rdr));
+				elf.e_shnum     = try!(elf.read_u16(rdr));
+				elf.e_shstrndx  = try!(elf.read_u16(rdr));
+			},
+
+			ELFCLASS64 => {
+				elf.e_type      = try!(elf.read_u16(rdr));
+				elf.e_machine   = try!(elf.read_u16(rdr));
+				elf.e_version   = try!(elf.read_u32(rdr));
+				elf.e_entry     = try!(elf.read_u64(rdr));
+				elf.e_phoff     = try!(elf.read_u64(rdr));
+				elf.e_shoff     = try!(elf.read_u64(rdr));
+				elf.e_flags     = try!(elf.read_u32(rdr));
+				elf.e_ehsize    = try!(elf.read_u16(rdr));
+				elf.e_phentsize = try!(elf.read_u16(rdr));
+				elf.e_phnum     = try!(elf.read_u16(rdr));
+				elf.e_shentsize = try!(elf.read_u16(rdr));
+				elf.e_shnum     = try!(elf.read_u16(rdr));
+				elf.e_shstrndx  = try!(elf.read_u16(rdr));
+			},
+
+			_ => {
+				return Err(ElfParseError::InvalidIdent);
+			},
+		}
+
+		if elf.e_version != (EV_CURRENT as u32) {
+			return Err(ElfParseError::InvalidVersion);
 		}
 
 		Ok(elf)
 	}
+
 }
 
 impl Loader for ElfFile {
 	fn entry_point(&self) -> u64 {
-		self.entry
+		self.e_entry
 	}
 }
 
